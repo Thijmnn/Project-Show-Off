e0789993e9997129e013b91e48d85f0bfa1daed8
Thijmen <thijmen.mennegat@gmail.com>
2025-05-19 13:19:45 +0200

    I hate how colliders work

--- Assets/Scripts/BubbleBehaviour.cs
+++ Assets/Scripts/BubbleBehaviour.cs
@@ -1,52 +1,62 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BubbleBehaviour : MonoBehaviour
 {
-    public bool DestroySelf;
+    [HideInInspector] public bool DestroySelf;
     private BubbleBehaviour _bubbleBehaviour;
+
+    Rigidbody _rb;
+    Collider _col;
+
+    public Collider[] playerColliders;
+    public Collider[] collObjs;
+    public List <Collider> ignoreColliders;
+
+    LayerMask layer = 6;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<Collider>();
+    }
+    public void Update()
+    {
+        _rb.mass = transform.localScale.x;
+
+        if(transform.localScale.x >= 20)
+        {
+            Destroy(gameObject);
+            print("bubblePopped!");
+        }
+
+        IgnoreColisions();
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.GetComponent<BubbleBehaviour>())
         {
-            _bubbleBehaviour = other.GetComponent<BubbleBehaviour>(); 
-            Vector3 distance = transform.position - other.transform.position;
-            float length = distance.magnitude;
-            float halfLength = (transform.localScale.x + other.transform.localScale.x) / 2;
+            _bubbleBehaviour = other.GetComponent<BubbleBehaviour>();
+            CheckOverlap(_bubbleBehaviour, other.gameObject);
+        }
+        
+    }
 
-            if(length <= (halfLength / 2))
-            {
-                if(other.transform.localScale.x > transform.localScale.x)
-                {
-                    Destroy(gameObject);
-                    other.transform.localScale = other.transform.localScale + (transform.localScale / 2);
-                }
-                else if(transform.localScale.x > other.transform.localScale.x)
-                {
-                    Destroy(other.gameObject);
-                    transform.localScale = transform.localScale + (other.transform.localScale / 2);
-                }
-                else if(other.transform.localScale.x == transform.localScale.x)
-                {
-                    if (DestroySelf == _bubbleBehaviour.DestroySelf)
-                    {
-                        RollRandom();
-                    }
-                    else if (DestroySelf && !_bubbleBehaviour.DestroySelf)
-                    {
-                        Destroy(gameObject);
-                        other.transform.localScale = other.transform.localScale + (transform.localScale / 2);
-                    }
-                    else if (_bubbleBehaviour.DestroySelf && !DestroySelf)
-                    {
-                        Destroy(other.gameObject);
-                        transform.localScale = transform.localScale + (other.transform.localScale / 2);
-                    }
-                }
-            }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            _col.isTrigger = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            _col.isTrigger = true;
         }
     }
 
@@ -67,9 +77,89 @@ public class BubbleBehaviour : MonoBehaviour
         }
     }
 
-    public void Update()
+
+
+    private void CheckOverlap(BubbleBehaviour _bubbleBehaviour, GameObject other)
     {
-        Rigidbody rig = GetComponent<Rigidbody>();
-        rig.mass = transform.localScale.x;
+        Vector3 distance = transform.position - other.transform.position;
+        float length = distance.magnitude;
+        float halfLength = (transform.localScale.x + other.transform.localScale.x) / 2;
+        Rigidbody rig = other.GetComponent<Rigidbody>();
+
+        if (length <= (halfLength / 2))
+        {
+            if (other.transform.localScale.x > transform.localScale.x)
+            {
+                Destroy(gameObject);
+                other.transform.localScale = other.transform.localScale + (transform.localScale / 2);
+                
+                rig.AddForce(_rb.velocity);
+            }
+            else if (transform.localScale.x > other.transform.localScale.x)
+            {
+                Destroy(other);
+                transform.localScale = transform.localScale + (other.transform.localScale / 2);
+            }
+            else if (other.transform.localScale.x == transform.localScale.x)
+            {
+                if (DestroySelf == _bubbleBehaviour.DestroySelf)
+                {
+                    RollRandom();
+                }
+                else if (DestroySelf && !_bubbleBehaviour.DestroySelf)
+                {
+                    Destroy(gameObject);
+                    other.transform.localScale = other.transform.localScale + (transform.localScale / 2);
+                    rig.AddForce(_rb.velocity);
+                }
+                else if (_bubbleBehaviour.DestroySelf && !DestroySelf)
+                {
+                    Destroy(other);
+                    transform.localScale = transform.localScale + (other.transform.localScale / 2);
+                }
+            }
+        }
+    }
+
+
+
+
+    void Awake()
+    {
+
+        playerColliders = GetComponents<Collider>();
+        
+        collObjs = FindObjectsOfType<Collider>();
+
+        foreach (Collider go in collObjs)
+        {
+            if (go.gameObject.layer == layer)
+            {
+                ignoreColliders.Add(go);
+            }
+        }
+    }
+
+    void IgnoreColisions()
+    {
+
+        
+        foreach (Collider playerColl in playerColliders)
+        {
+            
+            if (playerColl != null && playerColl.isTrigger == false)
+            {
+                
+                foreach (Collider ignoreColl in ignoreColliders)
+                {
+                    
+                    if (ignoreColl != null && ignoreColl.isTrigger == false)
+                    {
+                        
+                        Physics.IgnoreCollision(playerColl, ignoreColl, false);
+                    }
+                }
+            }
+        }
     }
 }
