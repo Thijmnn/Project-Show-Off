d4d163f0b43cd0933ae567582940d515cb00dd04
Thijmen <thijmen.mennegat@gmail.com>
2025-05-28 15:04:57 +0200

    Revert "Merge branch 'Nicola'"
    This reverts commit 6cad7cf8bcfcc383bf0fd5c7a711fe6cddf0f795, reversing
    changes made to 3807b0e4abff234e38848799fd35e827968d693c.

---
+++ Assets/Engineer/Scripts/SpeedBoost1.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpeedBoost1 : Wander
+{
+    public UnityEvent GiveSpeed;
+    bool canInteract = true;
+
+    float originalSpeed;
+    float originalSprintSpeed;
+    public float boostDuration;
+    public float moveSpeedIncrease;
+    public override void GiveBoost()
+    {
+        GiveSpeed.AddListener(IncreaseSpeed);
+        if (canInteract) { GiveSpeed?.Invoke(); canInteract = false; }
+        else
+        {
+            return;
+        }
+        
+    }
+
+    private void IncreaseSpeed()
+    {
+        StartCoroutine(SpeedIncrease(boostDuration, moveSpeedIncrease));
+    }
+
+    private IEnumerator SpeedIncrease(float boostDur, float speedInc)
+    {
+        originalSprintSpeed = _playerMovement.newSpeed;
+        originalSpeed = _playerMovement.originalSpeed;
+        _playerMovement.originalSpeed *= speedInc;
+        _playerMovement.newSpeed *= speedInc;
+
+        mesh.enabled = false;
+        yield return new WaitForSeconds(boostDur);
+
+        _playerMovement.newSpeed = originalSprintSpeed;
+        _playerMovement.originalSpeed = originalSpeed;
+
+        
+    }
+}
