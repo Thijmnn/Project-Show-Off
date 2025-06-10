fdda2a5d250881626bdfd84752554bf90ff823c9
SaraHesselink05 <548317@student.saxion.nl>
2025-06-10 15:53:24 +0200

    Ui buttons almost working?

--- Assets/Art/UIUX/Temp/optionsScript.cs
+++ Assets/Art/UIUX/Temp/optionsScript.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class optionsScript : MonoBehaviour
 {
 
     public UnityEvent optionsSwitch;
+    public InputActionReference menu;
+    private PlayerInput playerInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerInput = gameObject.GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(playerInput.actions["Menu"].triggered)
         {
             optionsSwitch.Invoke();
         }
