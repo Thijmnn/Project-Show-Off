3a31d27f418ced950c06ee82517f4b54e041dc02
KiomiDesigns <kiomitdamme@gmail.com>
2025-06-23 13:20:55 +0200

    Fix FMOD

---
+++ Assets/PlayerSounds.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField] private FMODUnity.EventReference _footsteps;
+    private FMOD.Studio.EventInstance footsteps;
+
+    private void Awake()
+    {
+        if (!_footsteps.IsNull)
+        {
+            footsteps = FMODUnity.RuntimeManager.CreateInstance(_footsteps);
+        }
+    }
+    public void PlayFootsteps()
+    {
+
+    }
+}
