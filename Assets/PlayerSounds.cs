e2313b851775c8461143d94b0677ce879e449cc5
KiomiDesigns <kiomitdamme@gmail.com>
2025-06-23 22:42:36 +0200

    klaar voor vandaag

--- Assets/PlayerSounds.cs
+++ Assets/PlayerSounds.cs
@@ -20,7 +20,35 @@ public class PlayerSounds : MonoBehaviour
         if (footsteps.isValid())
         {
             footsteps.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+
+            GroundSwitch();
             footsteps.start();
         }
     }
+    private void GroundSwitch()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position + Vector3.up * 0.5f, -Vector3.up);
+        Material surfaceMaterial;
+
+        if (Physics.Raycast(ray, out hit, 1.0f, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+        {
+            Renderer surfaceRenderer = hit.collider.GetComponentInChildren<Renderer>();
+            if (surfaceRenderer)
+            {
+               
+                Debug.Log(surfaceRenderer.material.name);
+                if (surfaceRenderer.material.name.Contains("grass"))
+                {
+                    
+                    footsteps.setParameterByName("Footsteps", 1);
+                }
+                else
+                {
+                    footsteps.setParameterByName("Footsteps", 0);
+                }
+            }
+        }
+    }
+
 }
