b63b25f445f808027d6b52f3ae037ffc69c3d335
Thijmen <thijmen.mennegat@gmail.com>
2025-06-25 14:49:02 +0200

    l

--- Assets/Engineer/Scripts/BlowingScript.cs
+++ Assets/Engineer/Scripts/BlowingScript.cs
@@ -56,16 +56,22 @@ public class BlowingScript : MonoBehaviour
         RemoveBubble(other.gameObject);
     }
 
-    private void Update()
+ /*   private void Update()
     {
         BlowBubbles();
 
 
         Blowing();
 
+    }*/
+
+    private void FixedUpdate()
+    {
+        BlowBubbles();
+
+
+        Blowing();
     }
-
-
     private void Blowing()
     {
         if (fireEnabled && bubblesInTrigger.Count > 0)
