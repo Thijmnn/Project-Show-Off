65eb3175b3ec1a09acb268ce88f16e50709bc200
KiomiDesigns <kiomitdamme@gmail.com>
2025-07-03 10:43:31 +0200

    fix bubble map 2 and collisions

--- Assets/Engineer/Scripts/SeedPlanter.cs
+++ Assets/Engineer/Scripts/SeedPlanter.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
